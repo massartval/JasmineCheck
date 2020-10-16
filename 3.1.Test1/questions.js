9f358828e6dce9a65680080e3beedc1932eac643
massartval <massart.val@gmail.com>
2020-10-16 11:34:41 +0200

    Completed 3.1 concatString

--- 3.1.Test1/questions.js
+++ 3.1.Test1/questions.js
@@ -1,62 +1,32 @@
-
 let tailleString = (texte) => {
-    return texte.length;
-}
+  return texte.length;
+};
 let remplaceECar = (texte) => {
-    let a = texte.indexOf("e")
-    let text =  texte.substr(" ", a) + " " + texte.substr(a + 1 , texte.length);
-    return text;
-}
+  let a = texte.indexOf("e");
+  let text = texte.substr(" ", a) + " " + texte.substr(a + 1, texte.length);
+  return text;
+};
 let concatString = (texte1, texte2) => {
+  let concatStr = `${texte1}${texte2}`;
+  return concatStr;
+};
+let afficherCar5 = (texte) => {};
+let afficher9Car = (texte) => {};
+let majusculeString = (texte) => {};
+let minusculeString = (texte) => {};
+let SupprEspaceString = (texte) => {};
+let IsString = (texte) => {};
 
-}
-let afficherCar5 =  (texte) => {
-
-}
-let afficher9Car =  (texte) => {
-
-}
-let majusculeString =  (texte) => {
-
-}
-let minusculeString =  (texte) => {
-
-}
-let SupprEspaceString =  (texte) => {
-
-}
-let IsString =  (texte) => {
-
-}
-
-let AfficherExtensionString =  (texte) => {
-
-}
-let NombreEspaceString =  (texte) => {
-
-}
-let InverseString =  (texte) => {
-
-}
+let AfficherExtensionString = (texte) => {};
+let NombreEspaceString = (texte) => {};
+let InverseString = (texte) => {};
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
-let calculPuissance =  (x, y) => {
-
-}
-let valeurAbsolue =  (nombre) => {
-
-}
-let valeurAbsolueArray =  (array) => {
-
-}
-let sufaceCercle =  (rayon) => {
-
-}
-let hypothenuse =  (ab, ac) => {
-
-}
-let calculIMC =  (poids, taille) => {
-
-}
+let calculPuissance = (x, y) => {};
+let valeurAbsolue = (nombre) => {};
+let valeurAbsolueArray = (array) => {};
+let sufaceCercle = (rayon) => {};
+let hypothenuse = (ab, ac) => {};
+let calculIMC = (poids, taille) => {};
